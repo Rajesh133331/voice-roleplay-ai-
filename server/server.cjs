9c145a1c0a3b015cacec80d0e6ed888ee624dc91
Rajesh133331 <104456124+Rajesh133331@users.noreply.github.com>
2026-04-16 14:01:47 +0530

    Update server.cjs

--- server/server.cjs
+++ server/server.cjs
@@ -188,11 +188,6 @@ app.use((req, res) => {
 
 
 
-app.use(express.static(path.join(__dirname, "../dist")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../dist/index.html"));
-});
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
